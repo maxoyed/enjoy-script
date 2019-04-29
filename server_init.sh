2f41067ce7d8ed4f222d59c0c6b2a9624ea5b39b
maxoyed <maxoyed@gmail.com>
2019-04-29 16:02:23 +0800

    add server_init.sh

---
+++ server_init.sh
@@ -0,0 +1,13 @@
+# install nginx
+apt install -y curl gnupg2 ca-certificates lsb-release && \
+echo "deb http://nginx.org/packages/debian `lsb_release -cs` nginx" \
+    | tee /etc/apt/sources.list.d/nginx.list && \
+curl -fsSL https://nginx.org/keys/nginx_signing.key | apt-key add - && \
+apt update && \
+apt install nginx && \
+
+# install docker
+curl -sSL https://get.docker.com/ | sh
+
+# install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
