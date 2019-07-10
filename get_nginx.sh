b0c66f1401a4ce4e01e42d6849600fd4a1a15626
maxoyed <maxoyed@gmail.com>
2019-07-11 02:54:46 +0800

    Add get_nginx.sh

---
+++ get_nginx.sh
@@ -0,0 +1,7 @@
+sudo apt install curl gnupg2 ca-certificates lsb-release -y
+echo "deb http://nginx.org/packages/debian `lsb_release -cs` nginx" \
+    | sudo tee /etc/apt/sources.list.d/nginx.list
+curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
+sudo apt-key fingerprint ABF5BD827BD9BF62
+sudo apt update
+sudo apt install nginx
