b4295e3fe312c5047216d1050454795b70b82182
maxoyed <maxoyed@gmail.com>
2019-07-11 03:15:31 +0800

    Fix install_nginx_docker.sh for CentOS7

--- CentOS/install_nginx_docker.sh
+++ CentOS/install_nginx_docker.sh
@@ -1,6 +1,6 @@
 # install nginx
-yum install -y yum-utils && \
-cat <<EOF> /etc/yum.repos.d/nginx.repo
+yum install -y yum-utils;<<EOF
+cat>/etc/yum.repos.d/nginx.repo
 [nginx-stable]
 name=nginx stable repo
 baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
@@ -14,7 +14,8 @@ baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
 gpgcheck=1
 enabled=0
 gpgkey=https://nginx.org/keys/nginx_signing.key
-EOF;
+EOF
+
 yum-config-manager --enable nginx-mainline && \
 yum install nginx;
 
